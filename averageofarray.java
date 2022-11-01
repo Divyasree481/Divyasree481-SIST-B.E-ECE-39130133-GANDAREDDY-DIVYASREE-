39aa39b297d36397e61a696b75dedf45615acf19
Divyasree481 <117014610+Divyasree481@users.noreply.github.com>
2022-11-01 21:24:38 +0530

    Add files via upload

---
+++ averageofarray.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.*;
+public class Main
+{
+
+   
+  public static void main(String[] args) {
+    ArrayList<Integer> li = new ArrayList<Integer>(); 
+    Scanner sc=new Scanner(System.in);
+    
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++){
+        i=sc.nextInt();
+        li.add(i);
+    }
+    int total = 0;
+    int avg;
+    for(int i = 0; i < li.size(); i++)
+    
+        total += li.get(i);
+        avg = total / li.size(); 
+        System.out.println("The Average is: " + avg);
+    
+  }
+}
